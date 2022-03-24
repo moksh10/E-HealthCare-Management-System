7d2ba43cee1ea2f15c6e7f83d4cf6e6747244a81
MOKSH <mokshteng@gmail.com>
2022-03-24 11:47:14 +0530

    project started and dependencies installed

---
+++ src/main/java/com/ehcare/ehcare/EhcareApplication.java
@@ -0,0 +1,13 @@
+package com.ehcare.ehcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehcare/ehcare/EhcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehcare.ehcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
