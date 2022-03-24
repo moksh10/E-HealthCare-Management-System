9d0d965385d410f8d822b9725f4119c4052467c1
MOKSH <mokshteng@gmail.com>
2022-03-24 17:37:56 +0530

    Appointment and MedicalRecord Entities created

---
+++ src/main/java/com/ehcare/ehcare/entities/Appointment.java
@@ -0,0 +1,121 @@
+package com.ehcare.ehcare.entities;
+
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="appointment")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler","patient","doctor"})
+public class Appointment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="appointmentID")
+	private int appointmentID;
+	
+	
+	@Column(name="appointmentDate")
+	@NotEmpty(message = "Appointment date required")
+	private Date appointmentDate;
+	
+	
+	@Column(name="healthProblem")
+	@NotEmpty(message = "Description of health problem required")
+	@Size(max=750,message = "Maximum length of description of health problem exceeded")
+	private String healthProblem;
+	
+	@Column(name="appointmentStatus")
+	@NotEmpty(message = "Appointment status required")
+	@Size(max=30,message = "Maximum length of appointment status exceeded")
+	private String appointmentStatus;
+	
+	@ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="patientID")
+	private Patient patient;
+	
+	@ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="doctorID")
+	private Doctor doctor;
+	
+	public Appointment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public Appointment(Date appointmentDate,String healthProblem,String appointmentStatus,Patient patient, Doctor doctor) {
+		this.appointmentDate = appointmentDate;
+		this.healthProblem = healthProblem;
+		this.appointmentStatus = appointmentStatus;
+		this.patient = patient;
+		this.doctor = doctor;
+	}
+
+
+
+	public int getAppointmentID() {
+		return appointmentID;
+	}
+
+	public void setAppointmentID(int appointmentID) {
+		this.appointmentID = appointmentID;
+	}
+
+	public Date getAppointmentDate() {
+		return appointmentDate;
+	}
+
+
+
+	public void setAppointmentDate(Date appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+
+
+	public String getHealthProblem() {
+		return healthProblem;
+	}
+
+	public void setHealthProblem(String healthProblem) {
+		this.healthProblem = healthProblem;
+	}
+
+	public String getAppointmentStatus() {
+		return appointmentStatus;
+	}
+
+	public void setAppointmentStatus(String appointmentStatus) {
+		this.appointmentStatus = appointmentStatus;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	
+}
---
+++ src/main/java/com/ehcare/ehcare/entities/MedicalRecord.java
@@ -0,0 +1,114 @@
+package com.ehcare.ehcare.entities;
+
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="medicalRecord")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler","patient","doctor"})
+public class MedicalRecord {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="medicalRecordID")
+	private int medicalRecordID;
+	
+	@Column(name="medicalRecordDate")
+	@NotEmpty(message = "Medical Record date required")
+	private Date medicalRecordDate;
+	
+	@Column(name="medicalRecordDiagnosis")
+	@NotEmpty(message = "Medical Record diagnosis required")
+	@Size(max=1800,message = "Maximum length of medical record diagnosis exceeded")
+	private String medicalRecordDiagnosis;
+	
+	@Column(name="medicalRecordDrugs")
+	@NotEmpty(message = "Medical Record drugs required")
+	@Size(max=700,message = "Maximum length of medical record drugs exceeded")
+	private String medicalRecordDrugs;
+	
+	@ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="patientID")
+	private Patient patient;
+	
+	@ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="doctorID")
+	private Doctor doctor;
+	
+	public MedicalRecord() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MedicalRecord(Date medicalRecordDate,String medicalRecordDiagnosis,String medicalRecordDrugs,Patient patient, Doctor doctor) {
+		this.medicalRecordDate = medicalRecordDate;
+		this.medicalRecordDiagnosis = medicalRecordDiagnosis;
+		this.medicalRecordDrugs = medicalRecordDrugs;
+		this.patient = patient;
+		this.doctor = doctor;
+	}
+
+	public int getMedicalRecordID() {
+		return medicalRecordID;
+	}
+
+	public void setMedicalRecordID(int medicalRecordID) {
+		this.medicalRecordID = medicalRecordID;
+	}
+
+	public Date getMedicalRecordDate() {
+		return medicalRecordDate;
+	}
+
+	public void setMedicalRecordDate(Date medicalRecordDate) {
+		this.medicalRecordDate = medicalRecordDate;
+	}
+
+	public String getMedicalRecordDiagnosis() {
+		return medicalRecordDiagnosis;
+	}
+
+	public void setMedicalRecordDiagnosis(String medicalRecordDiagnosis) {
+		this.medicalRecordDiagnosis = medicalRecordDiagnosis;
+	}
+
+	public String getMedicalRecordDrugs() {
+		return medicalRecordDrugs;
+	}
+
+	public void setMedicalRecordDrugs(String medicalRecordDrugs) {
+		this.medicalRecordDrugs = medicalRecordDrugs;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	
+	
+}
+
+	
