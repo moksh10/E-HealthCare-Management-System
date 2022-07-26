488acb7058651523774e470db4eeb60c669261a6
MOKSH <mokshteng@gmail.com>
2022-07-26 11:28:04 +0530

    bugs fixed

--- src/main/java/com/ehcare/ehcare/controllers/AuthController.java
+++ src/main/java/com/ehcare/ehcare/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.ehcare.ehcare.controllers;
 
-import java.util.Collection;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
